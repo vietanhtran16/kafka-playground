c35de95d94c6c2a5d765b8f702e804f229e93544
Viet Anh Tran <viet.anhtran@cultureamp.com>
2023-06-30 12:09:03 +1200

    Simple consumer and producer

---
+++ apps/consumer/src/index.js
@@ -0,0 +1,12 @@
+import { Consumer, Offset, KafkaClient } from "kafka-node";
+
+const topic = "convos";
+const client = new KafkaClient({});
+const topics = [{ topic }];
+const options = { fetchMaxWaitMs: 1000, fetchMaxBytes: 1024 * 1024 };
+
+const consumer = new Consumer(client, topics, options);
+
+consumer.on("message", (message) => {
+  console.log(message);
+});
---
+++ apps/producer/src/index.js
@@ -0,0 +1,18 @@
+import { Producer, KafkaClient, KeyedMessage } from "kafka-node";
+
+const topic = "convos";
+const client = new KafkaClient({});
+const producer = new Producer(client, { requireAcks: 1 });
+
+producer.on("ready", () => {
+  const keyedMessage = new KeyedMessage("keyed", "yeah nah mate");
+
+  producer.send([{ topic, messages: [keyedMessage] }], (err, result) => {
+    console.log(err || result);
+    process.exit();
+  });
+});
+
+producer.on("error", (err) => {
+  console.log("error", err);
+});
